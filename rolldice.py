e269087fb0723041982e4cafd20e1c3beb0fb845
AlexanderHayes1 <52278958+AlexanderHayes1@users.noreply.github.com>
2019-07-11 10:30:27 +0100

    Add files via upload

---
+++ rolldice.py
@@ -0,0 +1,7 @@
+import random
+BD = [1,2,3,4,ouzthat,5,6]
+UD = ['bowled','stumped','caught','not out','no ball','LBW']
+if BD == 'ouzthat':
+    random.choice(UB)=result2
+else:
+    random.choice(BD)=result1
